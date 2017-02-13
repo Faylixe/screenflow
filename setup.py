448eb48cb27ddee29e349ad69cceb4ef683f0436
Faylixe <felix.voituret@gmail.com>
2017-02-13 18:12:48 +0100

    Added distribution files.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+""" Setup module for ScreenFlow """
+
+from distutils.core import setup
+
+setup(
+    name='screenflow',
+    packages=['screenflow'],
+    version='1.0',
+    description='Light UI engine built on top of Pygame.',
+    author='Felix Voituret',
+    author_email='felix.voituret@gmail.com',
+    url='https://github.com/Faylixe/screenflow',
+    download_url='https://github.com/Faylixe/screenflow/tarball/1.0',
+    install_requires=['pygame'],
+    keywords=['pygame', 'UI'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: pygame'
+    ],
+    include_package_data=True
+)
